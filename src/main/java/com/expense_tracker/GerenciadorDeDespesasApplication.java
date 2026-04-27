23a28c797abc4731fa52b3388b7dfed81988dae3
rosadavi <davi.rosa.ofc@gmail.com>
2026-04-27 15:33:37 -0300

    first commit

---
+++ src/main/java/com/expense_tracker/GerenciadorDeDespesasApplication.java
@@ -0,0 +1,13 @@
+package com.expense_tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciadorDeDespesasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciadorDeDespesasApplication.class, args);
+	}
+
+}
