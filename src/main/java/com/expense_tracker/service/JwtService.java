0b3895339aba5ed49be6f751cbf5607fb97a3fe7
rosadavi <davi.rosa.ofc@gmail.com>
2026-04-28 12:46:31 -0300

    fix: corrigir geração de token JWT e uso de chave
    - substituir uso de String por Key (hmacShaKeyFor)
    - remover métodos deprecated do jjwt
    - corrigir erro de WeakKey e Base64

--- src/main/java/com/expense_tracker/service/JwtService.java
+++ src/main/java/com/expense_tracker/service/JwtService.java
@@ -1,28 +1,35 @@
 package com.expense_tracker.service;
 
 import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Service;
 
+import java.nio.charset.StandardCharsets;
+import java.security.Key;
 import java.util.Date;
 
 @Service
 public class JwtService {
 
-    private final String SECRET = "SECRET_KEY_866";
+
+    private Key getKey() {
+        final String SECRET = "minha-chave-super-secreta-com-pelo-menos-32-bytes!!";
+        return Keys.hmacShaKeyFor(SECRET.getBytes(StandardCharsets.UTF_8));
+    }
 
     public String generateToken(String userId) {
         return Jwts.builder()
                 .setSubject(userId)
                 .setIssuedAt(new Date())
                 .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60))
-                .signWith(SignatureAlgorithm.HS256, SECRET)
+                .signWith(getKey())
                 .compact();
     }
 
     public String validateToken(String token) {
-        return Jwts.parser()
-                .setSigningKey(SECRET)
+        return Jwts.parserBuilder()
+                .setSigningKey(getKey())
+                .build()
                 .parseClaimsJws(token)
                 .getBody()
                 .getSubject();
